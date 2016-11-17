4e9904db69813ec9575a93d8996a646bd9e992ac
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2016-11-17 17:07:33 -0500

    Edit tester to animate with a flow function
    - first_iteration_tester.m needs to be edited with the name of the
      flow function

--- first_iteration_tester.m
+++ first_iteration_tester.m
@@ -1,1 +1,13 @@
 % FIRST_ITERATION_TESTER  plots animation of swinging pendulum
+
+init_values = [-1, -1, 0, 0]; % position, in meters
+time_limits = [0 5]; % in seconds
+
+flow_handle = ; % handle for flow function
+% events_handle = ; % handle for events function
+
+[times, output] = ode45(flow_handle, time_limits, init_values);
+
+X = output(:,1);
+Y = output(:,2);
+comet(X,Y);
