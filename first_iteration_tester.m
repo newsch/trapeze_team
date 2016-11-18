9f4ddcfd426e19c5c5ef72c6bd2b9c8689b9f5ae
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2016-11-18 11:28:45 -0500

    Alter first_iteration_tester to be a function
    - first_iteration_tester.m now accepts a flow function handle and
      an optional event function handle

--- first_iteration_tester.m
+++ first_iteration_tester.m
@@ -1,13 +1,25 @@
+function first_iteration_tester(flow_handle, events_handle)
 % FIRST_ITERATION_TESTER  plots animation of swinging pendulum
+% events_handle is optional.
 
-init_values = [-1, -1, 0, 0]; % position, in meters
-time_limits = [0 5]; % in seconds
+    switch nargin
+    case 2
+      use_events = true;
+    otherwise
+      use_events = false;
+    end
 
-flow_handle = ; % handle for flow function
-% events_handle = ; % handle for events function
-
-[times, output] = ode45(flow_handle, time_limits, init_values);
-
-X = output(:,1);
-Y = output(:,2);
-comet(X,Y);
+    init_values = [-1, -1, 0, 0]; % position, in meters
+    time_limits = [0 5]; % in seconds
+    
+    if use_events
+        options = odeset('Events', events_handle);
+        [times, output] = ode45(flow_handle, time_limits, init_values, options);
+    else
+        [times, output] = ode45(flow_handle, time_limits, init_values);
+    end
+    
+    X = output(:,1);
+    Y = output(:,2);
+    comet(X,Y);
+end
