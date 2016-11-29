e6ba5ed2fc9b459d4bcb586ba36b83d2112f95bc
Evan New-Schmidt <eschmidt@olin.edu>
2016-11-29 16:52:09 -0500

    Separate handles for easy replacement

--- first_iteration_runner.m
+++ first_iteration_runner.m
@@ -1,13 +1,18 @@
 % FIRST_ITERATION_RUNNER  animates the first iteration
 
+% function handles
+first_trapeze_flow = @flows;
+release_event = @trapeze_event;
+after_release_flow = @released_flow;
+
 init_values = [-1, -1, 0, 0]; % position, in meters
 time_limits = [0 5]; % in seconds
 
-options = odeset('Events', @trapeze_event);
-[first_times, first_output] = ode45(@flows, time_limits, init_values, options);
+options = odeset('Events', release_event);
+[first_times, first_output] = ode45(first_trapeze_flow, time_limits, init_values, options);
 
 r_time_limits = [first_times(end), first_times(end) + 5];
-[second_times, second_output] = ode45(@released_flow, r_time_limits, first_output(end, :));
+[second_times, second_output] = ode45(after_release_flow, r_time_limits, first_output(end, :));
 
 times = [first_times(1:end - 1); second_times];
 output = [first_output(1:end - 1, :); second_output];
